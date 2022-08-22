4cb8f87134b22e64532fc50c1a3dd383fdaa0358
RafaelCardoso990 <rafaelcardoso990@gmail.com>
2022-08-22 14:53:25 -0300

    teste pr

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(json());
 
 app.get("/students", async (req: Request, res: Response) => {
+  console.log("students")
   const students = await prisma.student.findMany();
   res.send(students);
 });
